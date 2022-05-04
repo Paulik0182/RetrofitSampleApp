7540b16090498a67aeaf2069a3fac1866f6f2fc8
Paveł <Paulik0182@gmail.com>
2022-05-04 17:16:03 +0300

    Activity -> RecyclerView, ProgressBar. INTERNET. initView, showProgress, correction cod.

--- app/src/main/java/com/android/retrofitsampleapp/GitProjectEntity.java
+++ app/src/main/java/com/android/retrofitsampleapp/GitProjectEntity.java
@@ -1,4 +1,9 @@
 package com.android.retrofitsampleapp;
 
 public class GitProjectEntity {
+
+    private int id;
+    private String name;
+    private String description;
+
 }
--- app/src/main/java/com/android/retrofitsampleapp/MainActivity.java
+++ app/src/main/java/com/android/retrofitsampleapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.android.retrofitsampleapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.accessibility.AccessibilityManager;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -12,43 +14,73 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Retrofit retrofit = new Retrofit.Builder()
+    private final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())// это приобразователь объектов из одного типа в другой тип (здесь старонняя библиотека)
             .build();
 
-    private GitHubApi gitHubApi = retrofit.create(GitHubApi.class); //создаем gitHubApi. Автоматически обратится к интерфейсу
+    private final GitHubApi gitHubApi = retrofit.create(GitHubApi.class); //создаем gitHubApi. Автоматически обратится к интерфейсу
+
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+
+        loadProjects("borhammere");
+
         //многопоточность. Запуск паралельного потока
 //        new Thread(){
 //          //todo
 //        }.start();
+    }
 
-        gitHubApi.getProject("borhammere").enqueue(new Callback<List<GitProjectEntity>>() {
-           //получение ответа
+    private void loadProjects(String username) {
+        gitHubApi.getProject(username).enqueue(new Callback<List<GitProjectEntity>>() {
+            //получение ответа
             @Override
             public void onResponse(Call<List<GitProjectEntity>> call, Response<List<GitProjectEntity>> response) {
+                showProgress(false);
 //                if (response.code()==200){  //проверка кода, код 200 означает успех
-                if (response.isSuccessful()){ //isSuccessful - это уже проверка кодов от 200 до 300
+                if (response.isSuccessful()) { //isSuccessful - это уже проверка кодов от 200 до 300
                     List<GitProjectEntity> projects = response.body(); // body - это тело запроса, это будет список репозиториев которые мы ищем. Здесь мы получаем список проектов
+
+                    //test
+                    Toast.makeText(MainActivity.this, "Size" + projects.size(), Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "Error code" +response.code(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Error code" + response.code(), Toast.LENGTH_LONG).show();
                 }
             }
 
             //получение ошибки. чтото сломалось (нет сети и т.д.)
             @Override
             public void onFailure(Call<List<GitProjectEntity>> call, Throwable t) {
+                showProgress(false);
                 Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
-
             }
         });
+    }
+
+    private void initView() {
+        progressBar = findViewById(R.id.progress_bar);
+        recyclerView = findViewById(R.id.recycler_view);
+    }
+
+    private void showProgress(boolean showShow) {
+        if (showShow) {
+            recyclerView.setVisibility(View.GONE);//скрываем view со списком
+            progressBar.setVisibility(View.VISIBLE);//показываем прогресс загрузки
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);//показываем view со списком
+            progressBar.setVisibility(View.GONE);//скрываем прогресс загрузки
+        }
     }
 }
