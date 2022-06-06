e4820308cae3b54dee1073dc1853aa6141d9ffa4
Pavel <p21011982n@yandex.ru>
2022-06-06 10:32:36 +0300

    RoomGitUsersRepoImpl

---
+++ app/src/main/java/com/android/retrofitsampleapp/data/users/RoomGitUsersRepoImpl.java
@@ -0,0 +1,18 @@
+package com.android.retrofitsampleapp.data.users;
+
+import com.android.retrofitsampleapp.domain.users.GitUserEntity;
+import com.android.retrofitsampleapp.domain.users.GitUsersRepo;
+
+import java.util.List;
+
+public class RoomGitUsersRepoImpl implements GitUsersRepo {
+    @Override
+    public void saveUsers(List<GitUserEntity> users) {
+
+    }
+
+    @Override
+    public void getUsers(Callback callback) {
+
+    }
+}
